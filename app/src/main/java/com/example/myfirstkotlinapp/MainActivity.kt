58e6523a20669476434e4de720775a1610294738
varun_teja_boorla <varuntejaboorla@gmail.com>
2023-10-25 12:06:37 -0400

    project completed

---
+++ app/src/main/java/com/example/myfirstkotlinapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.myfirstkotlinapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.textView)
+        textView.text = "Hello Android,Varun Teja Boorla!"
+
+
+
+    }
+}
